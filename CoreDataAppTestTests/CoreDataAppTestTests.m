bd4af009b2cd2897adf689367711b86f7ac57968
frankregel <frank.regel@gmail.com>
2014-03-26 13:04:10 +0100

    Initial Commit

---
+++ CoreDataAppTestTests/CoreDataAppTestTests.m
@@ -0,0 +1,34 @@
+//
+//  CoreDataAppTestTests.m
+//  CoreDataAppTestTests
+//
+//  Created by Frank Regel on 26.03.14.
+//  Copyright (c) 2014 Frank Regel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CoreDataAppTestTests : XCTestCase
+
+@end
+
+@implementation CoreDataAppTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
